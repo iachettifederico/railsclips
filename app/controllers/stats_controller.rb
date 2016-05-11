52570bad4a0f090f4a188e5a5c28ec005f97d0f9
Federico Iachetti <iachetti.federico@gmail.com>
2016-05-11 17:50:04 -0300

    Removing stats

--- app/controllers/stats_controller.rb
+++
@@ -1,6 +0,0 @@
-class StatsController < ApplicationController
-  def posts
-    csv = CsvPostsStats.new(Post.all).render
-    send_data csv, filename: "posts.csv", type: 'text/csv', disposition: 'attachment'
-  end
-end
